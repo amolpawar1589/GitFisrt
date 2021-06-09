a7099806280609c30674ec5c866ea70ca6d02227
Amol <amolpawar1589@gmail.com>
2021-06-09 11:51:26 +0530

    add new fuction add

--- C#Test/TestCSharp/TestCSharp/Program.cs
+++ C#Test/TestCSharp/TestCSharp/Program.cs
@@ -8,13 +8,17 @@ namespace TestCSharp
         //implicit -which is declare by compiler when developer is not declare anything
         //explicit is 
         int i; string s; bool b;
-
+        public int add(int a,int b)
+        {
+            return (a + b);
+        }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             //explicit constructor
             Program p = new Program();
+            p.add(1,1);
             Console.WriteLine(p.i);
             Console.WriteLine(p.s);
             Console.WriteLine(p.b);
