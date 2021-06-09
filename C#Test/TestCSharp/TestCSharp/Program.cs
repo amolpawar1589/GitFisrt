5f3fb0f0e70fa97f95b2da78005aaa272c9151c0
Amol <amolpawar1589@gmail.com>
2021-06-09 13:15:04 +0530

    add new fuctions and lines

--- C#Test/TestCSharp/TestCSharp/Program.cs
+++ C#Test/TestCSharp/TestCSharp/Program.cs
@@ -8,29 +8,44 @@ namespace TestCSharp
         //implicit -which is declare by compiler when developer is not declare anything
         //explicit is 
         int i; string s; bool b;
-        public int add(int a,int b)
+        public int add(int a, int b)
         {
-            return (a + b);
+            return a + b;
         }
-        
-         public int Sub(int a,int b)
+
+        public int Sub(int a, int b)
         {
-            return (a - b);
+            return a - b;
         }
-        
-            public int Multi(int a,int b)
+
+        public int Multi(int a, int b)
         {
-            return (a * b);
-            }
+            return a * b;
+        }
+
+        public int div(int a, int b)
+        {
+            return a / b;
+        }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             //explicit constructor
             Program p = new Program();
-            p.add(1,1);
-            p.Sub(1,1);
-            p.Multi(1,1);
+            int total = 0;
+            total = Convert.ToInt32(p.add(1, 1));
+            Console.WriteLine("addition is  " + total);
+
+            total = Convert.ToInt32(p.Sub(1, 1));
+            Console.WriteLine("Substraction is  " + total);
+
+            total = Convert.ToInt32(p.Multi(1, 1));
+            Console.WriteLine("Multiplication  is  " + total);
+
+            total = Convert.ToInt32(p.div(1, 1));
+            Console.WriteLine("Division  is  " + total);
+
             Console.WriteLine(p.i);
             Console.WriteLine(p.s);
             Console.WriteLine(p.b);
