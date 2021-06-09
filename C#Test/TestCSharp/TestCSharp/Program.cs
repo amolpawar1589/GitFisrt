9bd253bb87fdec4b1369660b136c47c30a68dc61
Amol <amolpawar1589@gmail.com>
2021-06-09 11:38:19 +0530

    Initial commit

---
+++ C#Test/TestCSharp/TestCSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TestCSharp
+{
+    class Program
+    {
+        //constructor-Implict and explicit 
+        //implicit -which is declare by compiler when developer is not declare anything
+        //explicit is 
+        int i; string s; bool b;
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //explicit constructor
+            Program p = new Program();
+            Console.WriteLine(p.i);
+            Console.WriteLine(p.s);
+            Console.WriteLine(p.b);
+            Console.WriteLine(p.b);
+            Console.WriteLine(p.b);
+            Console.WriteLine(p.b);
+            Console.ReadLine();
+        }
+    }
+}
